45ba745896f16ce0ec0d576b633b0fc990cdc3dc
Pablo Vinícius <62220882+pablo1v@users.noreply.github.com>
2021-07-29 16:48:01 -0300

    chore: add new exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
+export * from './types';
+
 export * from './render';
 
-export * from './types';
+export * from './modules/Worker';
+export * from './modules/HandlerRunner';
+
+export * from './browser/BrowserManager';
+
+// Errors
+
+export * from './errors/BrowserError';
+export * from './errors/ErrorBase';
+export * from './errors/GenericError';
+export * from './errors/HandlerError';
+export * from './errors/HandlerOptionsError';
+export * from './errors/HandlerRunnerError';
