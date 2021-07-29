d32c5eb72fa657971ffffbf500683c3635e6c340
Pablo Vinícius <62220882+pablo1v@users.noreply.github.com>
2021-07-29 16:48:22 -0300

    clean

--- src/PDFMakerError.ts
+++
@@ -1,9 +0,0 @@
-export class PDFMakerError extends Error {
-  constructor(placeholder: string, message: string) {
-    super(message);
-
-    this.name = `PDFMakerError: ${placeholder}`;
-
-    Error.captureStackTrace(this, PDFMakerError);
-  }
-}
